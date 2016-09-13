f03b06374f5a90d0a151f7756d4d8e8f7d63f34b
ale <alessandro.saullo1@gmail.com>
2016-09-13 20:12:19 +0200

    Refactoring

--- proxy_server/enc_swift/config.py
+++ proxy_server/enc_swift/config.py
@@ -1,5 +1,5 @@
 AUTH_URL   = 'http://193.204.253.174:5000/v3'
-DAEMON_URL = 'http://193.204.253.174:8089'
+BARBICAN_URL = 'http://193.204.253.174:9311/v1'
 STORAGE_URL = 'http://193.204.253.174:8080/v1'
 #META_TENANT = 'meta_encswift'
 
