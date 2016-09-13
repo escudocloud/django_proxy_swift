8c684b66b87efe6a5d854378660d3876c12b108d
ale <alessandro.saullo1@gmail.com>
2016-09-13 19:06:40 +0200

    Separation proxy - browser

---
+++ swift_browser/swiftbrowser/config.py
@@ -0,0 +1,17 @@
+AUTH_URL   = 'http://127.0.0.1:8001'
+DAEMON_URL = 'http://193.204.253.174:8089'
+STORAGE_URL = 'http://127.0.0.1:8001'
+#META_TENANT = 'meta_encswift'
+
+SWIFT_USER = 'swift'
+
+PATH = 'swiftbrowser/enc_new'
+
+
+#True: Over-Encryption set
+#Anyother value = Over-Encryption not used
+OVER_ENCRYPTION = True
+
+
+
+
