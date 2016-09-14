afb0439d61b3136e01afa5fdb28e0a9ff0e0213a
ale <alessandro.saullo1@gmail.com>
2016-09-14 17:34:21 +0200

    Added comments

--- proxy_server/enc_swift/client.py
+++ proxy_server/enc_swift/client.py
@@ -15,24 +15,22 @@ class EncSwiftclient:
 
     def __init__(self, auth_token, project_id):
         auth_obj = v3.Token(auth_url=AUTH_URL, token=auth_token,  project_domain_name="Default",project_id=project_id)
-        #auth_obj = v3.Password(auth_url=AUTH_URL, username='enctest1',password='enctest1', project_domain_name="Default",user_domain_name="Default", project_name="demo")
         sess = session.Session(auth=auth_obj)
         #Store auth_token
         self.auth = auth_token
         #Retrieve tenant id
-        self.idtenant = project_id#sess.get_project_id()
-        
-        self.kc_conn = kc.Client(session=sess)
+        self.idtenant = project_id
         
         storage_url = '%s/AUTH_%s' %(STORAGE_URL,str(self.idtenant))
+        
+        self.kc_conn = kc.Client(session=sess)
         self.swift_conn = client.Connection(preauthtoken=auth_token,preauthurl=storage_url, auth_version='3')
-        # Connection to daemon only to retrieve users' ID 
+        
         self.iduser = sess.get_user_id()     
-        #self.iduser   = self.getUserID(user_name)
         self.SWIFT_ID = self.getUserID(SWIFT_USER)
                 
-        #Key manager instance 
-        self.sec_manager = sec_manager(auth_token,self.iduser)
+        #Secret manager instance 
+        self.sec_manager = sec_manager(sess,self.iduser)
 
     def getUserID(self,username):
         """
@@ -41,8 +39,6 @@ class EncSwiftclient:
         """
         ret_id = filter(lambda x: x.name == username, self.kc_conn.users.list())[0]
         return ret_id.id
-        #resp = sender().send_message('get_id','#'.join([self.auth,username]))
-        #return resp.content
 
     def getUsername(self,userid):
         """
@@ -51,8 +47,6 @@ class EncSwiftclient:
         """
         username = self.kc_conn.users.get(userid).name
         return username
-        #resp = sender().send_message('get_name','#'.join([self.auth,userid]))
-        #return resp.content
             
     def get_enc_object (self,container,obj):
         """
@@ -71,8 +65,6 @@ class EncSwiftclient:
             #Control ACL on the client side on private containers
             if actual_acl and (self.iduser not in actual_acl):
                 return         
-            
-            #catalog = self.sec_manager.load_catalog(self.iduser)
             
             hdrs, content = self.swift_conn.get_object(container,obj)
         except Exception,err:
@@ -104,16 +96,16 @@ class EncSwiftclient:
             return
 
         # Decrypt object with BEL dek
-        #try:
-        bel_DEK = self.sec_manager.get_secret(self.iduser,container_ref,object_bel_id).get('KEK',None)
-        if bel_DEK is not None:
-            logger.debug('Content encrypted with BEL >>> %s\n' % content)     
-            content = self.sec_manager.key_manager.decrypt_msg(str(content), bel_DEK)
-        logger.debug('Clear content >>> %s' % content)
-        return hdrs, str(content)
-        #except:
-        #    logger.error('Decrypt msg (BEL)')
-        #    return
+        try:
+            bel_DEK = self.sec_manager.get_secret(self.iduser,container_ref,object_bel_id).get('KEK',None)
+            if bel_DEK is not None:
+                logger.debug('Content encrypted with BEL >>> %s\n' % content)     
+                content = self.sec_manager.key_manager.decrypt_msg(str(content), bel_DEK)
+            logger.debug('Clear content >>> %s' % content)
+            return hdrs, str(content)
+        except:
+            logger.error('Decrypt msg (BEL)')
+            return
     
     def add_user_to_acl(self,headers,meta,usrID):
         """
@@ -152,13 +144,9 @@ class EncSwiftclient:
         except:
             #Create catalog node (new BEL DEK associated to the container)
             bel_DEK_id, obj = self.sec_manager.create_node(self.iduser, container)
-            # Send messages (for updating the graph)
+            # Store secrets (for updating the graph)
             container_ref = self.sec_manager.store_secrets(None,listACL,obj,bel_DEK_id)
             
-            #if self.send_message(listACL, obj, bel_DEK_id)!= 200:
-            #    logger.info('Error in send_message')
-            #    return
-
             # Create container
             try:
                 headers['x-container-meta-container-ref'] = container_ref
@@ -228,7 +216,6 @@ class EncSwiftclient:
             self.swift_conn.put_object(container, obj_name, enc_content,headers=obj_headers)
             logger.info("Object %s uploaded" % obj_name)
         except Exception,err:
-            print Exception,err
             logger.info("Error in Put Object")
 
     def postcontainer_public_to_private(self,container,headers,new_acl):
@@ -239,12 +226,11 @@ class EncSwiftclient:
         #Add self.iduser to new_acl
         new_acl.append(unicode(self.iduser))
         new_acl = list(set(new_acl))
+
         #Create a new BEL key
         bel_id, obj_bel = self.sec_manager.create_node(self.iduser, container)
-        # Send messages (for updating the graph)
-        #if self.send_message(new_acl, obj_bel, bel_id)!= 200:
-        #    logger.info('Error in send_message')
-        #    return
+  
+        #Store secrets
         container_ref = self.sec_manager.store_secrets(None,new_acl,obj_bel,bel_id)
         cont_headers={}
         cont_headers['x-container-meta-container-ref'] = container_ref
@@ -261,7 +247,6 @@ class EncSwiftclient:
                    head, content = self.swift_conn.get_object(container, obj['name'])
                    self.put_enc_object(container,obj['name'],content)  
         except Exception,err:
-            print Exception,err
             logger.info("Error in post container (become private)")
 
     def postcontainer_private_to_public(self,container,headers):
@@ -295,13 +280,13 @@ class EncSwiftclient:
         
         """
         #Retrieve all the BEL keys
-        
         dict_bel_DEKs = self.retrieve_bel_DEKs(container_name,cont_secret_ref)
+        
         #Add BEL container DEK if no object has been uploaded
         dict_bel_DEKs[actual_bel_id] = self.sec_manager.get_secret(self.iduser,cont_secret_ref,actual_bel_id)
         new_cont_secret_ref = cont_secret_ref
         for bel_DEK_id,obj in dict_bel_DEKs.items():
-            #Send messages
+            #Store secrets
             new_cont_secret_ref = self.sec_manager.store_secrets(cont_secret_ref,added_users,obj,bel_DEK_id)
         return new_cont_secret_ref
 
@@ -324,54 +309,52 @@ class EncSwiftclient:
         new_cont_secret_ref = cont_secret_ref
         removed_users = list(set(actual_acl).difference(new_acl))
         added_users   = list(set(new_acl).difference(actual_acl))
-        #try:
-        if added_users:
-            #Send the BEL DEKs protecting the objects included in the container, to all the added users
-            new_cont_secret_ref = self.store_actual_BEL_DEKs(cont_secret_ref,container_name,added_users,actual_head['x-container-meta-bel-id'])
-            headers['x-container-meta-container-ref'] = new_cont_secret_ref
-            if not removed_users and OVER_ENCRYPTION: 
-                if not set(new_acl).issuperset(set(initial_acl_sel)):
-                    #No change to the actual protection layers
-                    if initial_acl_sel:
-                        new_list = list(set(initial_acl_sel + added_users))
-                        headers['x-container-meta-sel-acl'] = str({self.idtenant : map(lambda x: "AUTH_" + str(x),new_list)})
-                    new_cont_secret_ref = self.sec_manager.store_secrets(new_cont_secret_ref,added_users,self.sec_manager.get_secret(self.iduser,new_cont_secret_ref,actual_sel_id),actual_sel_id)
-                    headers['x-container-meta-container-ref'] = new_cont_secret_ref
-                else:
-                    #Remove SEL protection (if exists)
-                    headers[u'x-container-meta-sel-id']= u''
-                    headers[u'x-container-meta-sel-acl']= u''
-                    headers[u'x-container-meta-sel-version']= u''
-                    #self.send_message(actual_acl + [self.SWIFT_ID],{},actual_sel_id)
-        if removed_users:
-            bel_id, obj_bel = self.sec_manager.create_node(self.iduser, container_name)
-            headers['x-container-meta-bel-id'] = str(bel_id)
-            new_cont_secret_ref = self.sec_manager.store_secrets(new_cont_secret_ref,new_acl,obj_bel,bel_id)
-            headers['x-container-meta-container-ref'] = new_cont_secret_ref
-            if not OVER_ENCRYPTION:
-                #Only BEL option: download all the files, re-encrypt and upload them 
-                self.swift_conn.post_container(container_name,headers=headers)
-                head, list_obj = self.get_container(container_name)
-                for obj in list_obj:
-                    if obj['name'][-1] != '/' or obj['content_type'] != 'application/directory':
-                        head, content = self.get_enc_object(container_name, obj['name'])
-                        self.put_enc_object(container_name,obj['name'],content)
-                return
-            if OVER_ENCRYPTION:
-                #Apply a new Surface Encryption Layer
-                sel_id, obj_sel = self.sec_manager.create_node(self.iduser, container_name)
-                init_acl = list(set(initial_acl_sel + added_users)) if initial_acl_sel else list(set(new_acl + actual_acl))
-                headers['x-container-meta-sel-id'] = str(sel_id)
-                headers['x-container-meta-sel-acl'] = str({self.idtenant:map(lambda x:"AUTH_"+str(x), init_acl)})
-                headers['x-container-meta-sel-version'] = str(eval(version_sel_DEK)+1)
-                new_cont_secret_ref = self.sec_manager.store_secrets(new_cont_secret_ref, new_acl+ [self.SWIFT_ID],obj_sel,sel_id)
+        try:
+            if added_users:
+                #Send the BEL DEKs protecting the objects included in the container, to all the added users
+                new_cont_secret_ref = self.store_actual_BEL_DEKs(cont_secret_ref,container_name,added_users,actual_head['x-container-meta-bel-id'])
                 headers['x-container-meta-container-ref'] = new_cont_secret_ref
-                #new_cont_secret_ref = self.send_message(new_cont_secret_ref, actual_acl + [self.SWIFT_ID],{},actual_sel_id)!= 200:
-        self.swift_conn.post_container(container_name,headers=headers)
-        """except Exception, err:
-            print Exception,err
+                if not removed_users and OVER_ENCRYPTION: 
+                    if not set(new_acl).issuperset(set(initial_acl_sel)):
+                        #No change to the actual protection layers
+                        if initial_acl_sel:
+                            new_list = list(set(initial_acl_sel + added_users))
+                            headers['x-container-meta-sel-acl'] = str({self.idtenant : map(lambda x: "AUTH_" + str(x),new_list)})
+                        new_cont_secret_ref = self.sec_manager.store_secrets(new_cont_secret_ref,added_users,self.sec_manager.get_secret(self.iduser,new_cont_secret_ref,actual_sel_id),actual_sel_id)
+                        headers['x-container-meta-container-ref'] = new_cont_secret_ref
+                    else:
+                        #Remove SEL protection (if exists)
+                        headers[u'x-container-meta-sel-id']= u''
+                        headers[u'x-container-meta-sel-acl']= u''
+                        headers[u'x-container-meta-sel-version']= u''
+                        #self.send_message(actual_acl + [self.SWIFT_ID],{},actual_sel_id)
+            if removed_users:
+                bel_id, obj_bel = self.sec_manager.create_node(self.iduser, container_name)
+                headers['x-container-meta-bel-id'] = str(bel_id)
+                new_cont_secret_ref = self.sec_manager.store_secrets(new_cont_secret_ref,new_acl,obj_bel,bel_id)
+                headers['x-container-meta-container-ref'] = new_cont_secret_ref
+                if not OVER_ENCRYPTION:
+                    #Only BEL option: download all the files, re-encrypt and upload them 
+                    self.swift_conn.post_container(container_name,headers=headers)
+                    head, list_obj = self.get_container(container_name)
+                    for obj in list_obj:
+                        if obj['name'][-1] != '/' or obj['content_type'] != 'application/directory':
+                            head, content = self.get_enc_object(container_name, obj['name'])
+                            self.put_enc_object(container_name,obj['name'],content)
+                    return
+                if OVER_ENCRYPTION:
+                    #Apply a new Surface Encryption Layer
+                    sel_id, obj_sel = self.sec_manager.create_node(self.iduser, container_name)
+                    init_acl = list(set(initial_acl_sel + added_users)) if initial_acl_sel else list(set(new_acl + actual_acl))
+                    headers['x-container-meta-sel-id'] = str(sel_id)
+                    headers['x-container-meta-sel-acl'] = str({self.idtenant:map(lambda x:"AUTH_"+str(x), init_acl)})
+                    headers['x-container-meta-sel-version'] = str(eval(version_sel_DEK)+1)
+                    new_cont_secret_ref = self.sec_manager.store_secrets(new_cont_secret_ref, new_acl+ [self.SWIFT_ID],obj_sel,sel_id)
+                    headers['x-container-meta-container-ref'] = new_cont_secret_ref
+                    #new_cont_secret_ref = self.send_message(new_cont_secret_ref, actual_acl + [self.SWIFT_ID],{},actual_sel_id)!= 200:
+            self.swift_conn.post_container(container_name,headers=headers)
+        except Exception, err:
             logger.debug("Error in Post container.")
-            raise Exception"""
 
     def post_enc_container (self,container,headers):
         """
@@ -467,9 +450,17 @@ class EncSwiftclient:
         return acl_clean
 
     def get_container (self, container, marker=None, delimiter=None, prefix=None):
+        """
+        Get a specific container
+        Args:
+            container: the name of the container
+        """
         return self.swift_conn.get_container(container, marker=marker, delimiter=delimiter, prefix=prefix)
 
     def get_account (self):
+        """
+        Get all information about an account
+        """
         account, containers = self.swift_conn.get_account()
         list_cont = []
         for cont in containers:
@@ -481,12 +472,6 @@ class EncSwiftclient:
         return account, list_cont
 
     def delete_object(self, container, obj):
-        """if obj[-1] == '/':# or obj['content_type'] != 'application/directory':
-            meta, objects = self.get_container(container, marker=None, delimiter='/', prefix=obj)
-            if len(objects) > 1:
-                # pseudofolder is not empty
-                raise Exception
-                return"""
         return self.swift_conn.delete_object(container, obj)
 
     def head_object(self, container, obj):
--- proxy_server/enc_swift/config.py
+++ proxy_server/enc_swift/config.py
@@ -1,12 +1,10 @@
 AUTH_URL   = 'http://193.204.253.174:5000/v3'
 BARBICAN_URL = 'http://193.204.253.174:9311/v1'
 STORAGE_URL = 'http://193.204.253.174:8080/v1'
-#META_TENANT = 'meta_encswift'
 
 SWIFT_USER = 'swift'
 
 PATH = 'enc_swift'
-
 
 #True: Over-Encryption set
 #Anyother value = Over-Encryption not used
