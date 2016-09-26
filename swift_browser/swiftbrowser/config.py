a38e5efcfc4cd55c4bc7eb453043114b76f049d7
alessandrosaullo <alessandro.saullo1@gmail.com>
2016-09-26 15:49:40 +0200

    Refactoring client2

--- swift_browser/swiftbrowser/config.py
+++ swift_browser/swiftbrowser/config.py
@@ -1,6 +1,5 @@
-AUTH_URL   = 'http://127.0.0.1:8001'
-DAEMON_URL = 'http://193.204.253.174:8089'
-STORAGE_URL = 'http://127.0.0.1:8001'
+AUTH_URL   = 'http://127.0.0.1:8801'
+STORAGE_URL = 'http://127.0.0.1:8801'
 #META_TENANT = 'meta_encswift'
 
 SWIFT_USER = 'swift'
